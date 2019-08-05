7432971f02bfeda1e3c027838fc2482302e907d2
routson01 <HenryRo@bgs.vic.ed.au>
2019-08-05 22:32:27 +1000

    Testing files, context menu and General changes

--- PythonFiles/UserInterface.py
+++ PythonFiles/UserInterface.py
@@ -2,10 +2,15 @@
 
 """
 to do :
-fix context menu functions
-closeall after main
+expand click for index 
+fix max and ideal
 animation matplots
 ideals out of frame
+inherit
+closeall after main
+import changes
+pep8 http://pep8online.com/checkresult
+pure functions
 comments
 """
 
@@ -29,19 +34,9 @@ matplotlib.use('TkAgg') # speed increase
 import Adviser
 import Files
 
-# import changes
-# pep8 http://pep8online.com/checkresult
-# pure functions
-
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-
-        # globals
-        self.color = "white" # opposite
-        self.font = QFont()
-        self.text_size = 9
-        # text color
 
         # param
         self.UI = UI(self)
@@ -49,7 +44,6 @@ class MainWindow(QMainWindow):
         size = app.primaryScreen().size()
         self.setMinimumSize(size.width(), size.height()/2)
         self.showMaximized()
-        self.Light_Dark()
         self.setWindowTitle('GUI')
         self.setWindowIcon(QIcon('pythonlogo.png'))
 
@@ -76,13 +70,17 @@ class MainWindow(QMainWindow):
         SettingsMenu = MainMenu.addMenu('Settings')
 
         text_size_Up = QAction("Text Size Up",self)
-        text_size_Up.triggered.connect(self.change_text_size)
+        text_size_Up.triggered.connect(self.text_size)
         SettingsMenu.addAction(text_size_Up)
 
+        self.font = QFont()
+        self.text_size_value = 9
         text_size_Down = QAction("Text Size Down",self)
-        text_size_Down.triggered.connect(self.change_text_size)
+        text_size_Down.triggered.connect(self.text_size)
         SettingsMenu.addAction(text_size_Down)
 
+        self.color = ""
+        self.Light_Dark()
         Light_Dark = QAction("Light/Dark",self)
         Light_Dark.triggered.connect(self.Light_Dark)
         SettingsMenu.addAction(Light_Dark)
@@ -95,13 +93,8 @@ class MainWindow(QMainWindow):
         HelpMenu.addAction(Documentation)
 
         # testing
-        # self.Open(['C:/Users/henryro/OneDrive - Ballarat Grammar School/2019 Software/Sat/ExcelFiles/Compost 15mm 2018.xlsx', 'C:/Users/henryro/OneDrive - Ballarat Grammar School/2019 Software/Sat/ExcelFiles/Compost 15mmm 2018_Office.xlsx', 'C:/Users/henryro/OneDrive - Ballarat Grammar School/2019 Software/Sat/ExcelFiles/Compost 25mm 2018.xlsx', 'C:/Users/henryro/OneDrive - Ballarat Grammar School/2019 Software/Sat/ExcelFiles/Compost Geelong Sample.xlsx', 'C:/Users/henryro/OneDrive - Ballarat Grammar School/2019 Software/Sat/ExcelFiles/Ideal.xlsx', 'C:/Users/henryro/OneDrive - Ballarat Grammar School/2019 Software/Sat/ExcelFiles/Soil N.Cole Dam.xlsx', 'C:/Users/henryro/OneDrive - Ballarat Grammar School/2019 Software/Sat/ExcelFiles/Soil N.Cole Elephant Track.xlsx', 'C:/Users/henryro/OneDrive - Ballarat Grammar School/2019 Software/Sat/ExcelFiles/Soil N.Cole Little.xlsx', 'C:/Users/henryro/OneDrive - Ballarat Grammar School/2019 Software/Sat/ExcelFiles/Soil N.Cole Mail Box.xlsx', 'C:/Users/henryro/OneDrive - Ballarat Grammar School/2019 Software/Sat/ExcelFiles/Soil N.Cole School.xlsx'])
-        self.Open(['C:/Users/henryro/OneDrive - Ballarat Grammar School/2019 Software/Sat/ExcelFilesTesting/Compost.xlsx', 'C:/Users/henryro/OneDrive - Ballarat Grammar School/2019 Software/Sat/ExcelFilesTesting/Ideal.xlsx', 'C:/Users/henryro/OneDrive - Ballarat Grammar School/2019 Software/Sat/ExcelFilesTesting/Soil.xlsx'])
-
-    def refresh(self):
-        self.UI.setStyleSheet("font: "+str(self.text_size)+"pt")
-        rcParams.update({'font.size': self.text_size + 3})
-        self.UI.update_graph()
+        self.Open(['C:/Users/henryro/OneDrive - Ballarat Grammar School/2019 Software/Sat/ExcelFiles/Compost 15mm 2018.xlsx', 'C:/Users/henryro/OneDrive - Ballarat Grammar School/2019 Software/Sat/ExcelFiles/Compost 15mmm 2018_Office.xlsx', 'C:/Users/henryro/OneDrive - Ballarat Grammar School/2019 Software/Sat/ExcelFiles/Compost 25mm 2018.xlsx', 'C:/Users/henryro/OneDrive - Ballarat Grammar School/2019 Software/Sat/ExcelFiles/Compost Geelong Sample.xlsx', 'C:/Users/henryro/OneDrive - Ballarat Grammar School/2019 Software/Sat/ExcelFiles/Ideal.xlsx', 'C:/Users/henryro/OneDrive - Ballarat Grammar School/2019 Software/Sat/ExcelFiles/Soil N.Cole Dam.xlsx', 'C:/Users/henryro/OneDrive - Ballarat Grammar School/2019 Software/Sat/ExcelFiles/Soil N.Cole Elephant Track.xlsx', 'C:/Users/henryro/OneDrive - Ballarat Grammar School/2019 Software/Sat/ExcelFiles/Soil N.Cole Little.xlsx', 'C:/Users/henryro/OneDrive - Ballarat Grammar School/2019 Software/Sat/ExcelFiles/Soil N.Cole Mail Box.xlsx', 'C:/Users/henryro/OneDrive - Ballarat Grammar School/2019 Software/Sat/ExcelFiles/Soil N.Cole School.xlsx'])
+        # self.Open(['C:/Users/henryro/OneDrive - Ballarat Grammar School/2019 Software/Sat/ExcelFilesTesting/Compost.xlsx', 'C:/Users/henryro/OneDrive - Ballarat Grammar School/2019 Software/Sat/ExcelFilesTesting/Ideal.xlsx', 'C:/Users/henryro/OneDrive - Ballarat Grammar School/2019 Software/Sat/ExcelFilesTesting/Soil.xlsx'])
 
     def Open(self, full_paths):
         if full_paths == False:
@@ -114,30 +107,30 @@ class MainWindow(QMainWindow):
                 self.UI.soil_link = full_path
             else:
                 self.UI.create_slider(full_path)
-        self.refresh()
+        self.UI.update_graph()
 
     def DragAndDrop(self):
         self.DragDrop = DragDrop()
-        self.refresh()
 
-    def change_text_size(self):
+    def text_size(self):
         change = 1
         try:
             if self.sender().text() == "Text Size Up":
-                self.text_size += change
+                self.text_size_value += change
             elif self.sender().text() == "Text Size Down":
                 if self.text_size>change:
-                    self.text_size += -change
+                    self.text_size_value += -change
         except:
             pass
-        self.refresh()
+        self.UI.setStyleSheet("font: "+str(self.text_size_value)+"pt")
+        rcParams.update({'font.size': self.text_size_value + 3})
+        self.UI.update_graph()
 
     def Light_Dark(self):
-        # change text
-        if self.color == "white":
-            self.color = "#3D3D3D"
+        if self.color == "#EBEBEB":
+            self.color = "#70706D"
         else:
-            self.color = "white"
+            self.color = "#EBEBEB"
         try:
             self.DragDrop.setStyleSheet("QWidget { background-color: "+self.color+" }")
         except:
@@ -146,20 +139,19 @@ class MainWindow(QMainWindow):
         self.UI.setStyleSheet("QWidget { background-color: "+self.color+" }")
         self.UI.ax.set_facecolor(self.color)
         self.UI.fig.set_facecolor(self.color)
-        self.refresh()
+        self.UI.update_graph()
 
 class QCustomSlider(QSlider):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.decimals = 2
-        self._max_int = 10 ** self.decimals
+
+        self._min_value = 0.00
+        self._max_value = 0.05
+        self._max_int = 1000
 
         super().setMinimum(0)
         super().setMaximum(self._max_int)
-
-        self._min_value = 0.0000001
-        self._max_value = 0.0500000
 
     def _value_range(self):
         return self._max_value - self._min_value
@@ -172,19 +164,22 @@ class QCustomSlider(QSlider):
 
     # https://github.com/PyQt5/PyQt/blob/master/QSlider/ClickJumpSlider.py
     def mousePressEvent(self, event):
-        option = QStyleOptionSlider()
-        self.initStyleOption(option)
-        rect = self.style().subControlRect(
-            QStyle.CC_Slider, option, QStyle.SC_SliderHandle, self)
-        if rect.contains(event.pos()):
-            super(QCustomSlider, self).mousePressEvent(event)
-            return
-        setValue = self.style().sliderValueFromPosition(
-            self.minimum(), self.maximum(),
-            (self.height() - event.y()) if not self.invertedAppearance(
-            ) else event.y(), self.height())
-        super().setValue(setValue)
-        
+        if event.button() == Qt.LeftButton:
+            option = QStyleOptionSlider()
+            self.initStyleOption(option)
+            rect = self.style().subControlRect(
+                QStyle.CC_Slider, option, QStyle.SC_SliderHandle, self)
+            if rect.contains(event.pos()):
+                super(QCustomSlider, self).mousePressEvent(event)
+                return
+            setValue = self.style().sliderValueFromPosition(
+                self.minimum(), self.maximum(),
+                (self.height() - event.y()) if not self.invertedAppearance( 
+                ) else event.y(), self.height())
+            super().setValue(setValue)
+        else:
+            MainWindow.UI.mousePressEvent(event)
+
 class UI(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -208,10 +203,12 @@ class UI(QWidget):
         self.slider_layout.addLayout(self.buttons)
         self.slider_layout.addLayout(self.values)
 
-        self.sliderLinks = []
+        self.slider_links = []
+        self.current_slider_links = []
         self.color_options = []
         self.colors = []
         self.color_pos = 0
+        self.index = 0
     
         # viridis for color blind
         cmap = cm.get_cmap('viridis', 8)
@@ -221,11 +218,12 @@ class UI(QWidget):
 
     def create_slider(self,path):
 
-        for x in self.sliderLinks:
+        for x in self.slider_links:
             if path == x:
                 return None
             
-        self.sliderLinks.append(path)
+        self.slider_links.append(path)
+        self.current_slider_links.append(path)
         slider = QSlider(Qt.Vertical)
         custom_slider = QCustomSlider(slider)
 
@@ -235,9 +233,10 @@ class UI(QWidget):
         self.color_pos += 1
         self.colors.append(hex_color)
         custom_slider.setStyleSheet("QSlider::handle:vertical {background-color: "+hex_color+";}")
-        custom_slider.valueChanged.connect(lambda: [self.update_graph(), self.update_index(custom_slider)]) # remove after animation
+        custom_slider.valueChanged.connect(lambda: self.update_graph()) # change after animation
         
         button = QPushButton()
+        button.setStyleSheet("QPushButton:hover:!pressed { border: 2px solid red; }")
         name = os.path.basename(path).replace(".xlsx","")
         name = name.replace(" ","\n")
         button.setText(name)
@@ -252,7 +251,7 @@ class UI(QWidget):
         self.buttons.itemAt(index).widget().setParent(None)
         self.sliders.itemAt(index).widget().setParent(None)
         self.values.itemAt(index).widget().setParent(None)
-        del self.sliderLinks[index]
+        del self.slider_links[index]
         del self.colors[index]
         self.update_graph()
 
@@ -272,12 +271,8 @@ class UI(QWidget):
         self.soil_link = r""
         self.ideal_link = r""
         self.update_graph()
-        # FuncAnimation(self.fig, update_graph)
 
     def update_graph(self):  
-        # use animation
-        # if avoids try but adds color bug
-        # shape mismatch
         try:
             self.soilValues, self.names = Files.run(self.soil_link)
         except:
@@ -295,23 +290,25 @@ class UI(QWidget):
         values_sum = self.soilValues
         for index in range(self.sliders.count()):
             slider_value = self.sliders.itemAt(index).widget().value()
-            ys = Files.run(self.sliderLinks[index])[0]*slider_value
+            ys = Files.run(self.slider_links[index])[0]*slider_value
             self.ax.bar(xs, ys, bottom=values_sum, color=self.colors[index], edgecolor='black')
             values_sum += ys
             T_Ha = round(1330*slider_value,1)
             self.values.itemAt(index).widget().setText(str(T_Ha)+"T/Ha")
 
-        self.ax.bar(xs, self.IdealValues, facecolor="None", edgecolor='green') # want out of frame
-        self.ax.bar(xs, self.IdealValues*4, facecolor="None", edgecolor='red') # want out of frame
+        self.ax.bar(xs, self.IdealValues, facecolor="None", edgecolor='green')
+        self.ax.bar(xs, self.IdealValues*4, facecolor="None", edgecolor='red')
         self.ax.set_xticks(xs)
         self.ax.set_xticklabels(self.names)
         self.ax.figure.canvas.draw()
 
-    def update_index(self, slider):
-        self.index = self.sliders.indexOf(slider)
-
     def context_menu_init(self):
         self.setContextMenuPolicy(Qt.ActionsContextMenu)
+        self.customContextMenuRequested.connect(self.contextMenuEvent)
+
+        sliders_reset = QAction("sliders reset", self)
+        sliders_reset.triggered.connect(self.sliders_reset)
+        self.addAction(sliders_reset)
         
         auto_ideal = QAction("auto ideal", self)
         auto_ideal.triggered.connect(self.auto_ideal)
@@ -321,53 +318,66 @@ class UI(QWidget):
         auto_max.triggered.connect(self.auto_max)
         self.addAction(auto_max)
 
-        sliders_reset = QAction("sliders reset", self)
-        sliders_reset.triggered.connect(self.sliders_reset)
-        self.addAction(sliders_reset)
+    # def mouseActivation(self):
+    #     print("mouseActivation")
 
-    def auto_ideal(self):
-        """
-        10 -> 7
-        vec  = 1.5
-        10 + -2 * 1.5
+    # def QContextMenuEvent(self, event):
+    #     print("QContextMenuEvent")
+    #     print(event)
 
-        7 -> 10
-        vec = 1.5
-        7 + 2 * 1.5
+    # def contextMenuEvent(self, event):
+    #     print("contextMenuEvent")
+    #     print(event)
 
-        start + scale * current vec
-        """
-        # get values
-        change_vector = self.IdealValues
-        sub_vectors = np.zeros(len(change_vector)) 
-        for i in range(len(self.sliderLinks)):
-            values = Files.run(self.sliderLinks[i])[0]
-            slider_value = self.sliders.itemAt(i).widget().value()
-            scaled_values = values * slider_value
-            change_vector -= scaled_values
-            sub_vectors = np.vstack((sub_vectors, scaled_values))
-        sub_vectors = sub_vectors[1:]
-        # run
-        index, scale = Adviser.run(change_vector,sub_vectors)
-        if scale == None:
-            return None
-        # update 
-        current = self.sliders.itemAt(index).widget().value()
-        self.sliders.itemAt(index).widget().setValue(scale*current)
-
-    def auto_max(self):
-        change_vector = self.IdealValues*4-self.soilValues
-        values = Files.run(self.sliderLinks[self.index])[0]
-        setValue = np.amin(change_vector/values)
-        self.sliders.itemAt(self.index).widget().setValue(setValue)
+    def mousePressEvent(self, event):
+        if event.button() == Qt.RightButton:
+            self.current_slider_links = self.slider_links
+            index = int( event.globalX() / 125 )
+            print(index)
+            if index < len(self.slider_links):
+                self.current_slider_links = [self.slider_links[index]]
 
     def sliders_reset(self):
-        for index in range(self.sliders.count()):
+        for i in range(len(self.current_slider_links)):
+            index = self.slider_links.index(self.current_slider_links[i])
             self.sliders.itemAt(index).widget().setValue(0)
-            
+
+    def auto_ideal(self):
+        # get soil
+        change_vector = self.IdealValues-self.soilValues
+        # get slider values
+        sub_vectors = np.zeros(len(change_vector)) 
+        for i in range(len(self.current_slider_links)):
+            values = Files.run(self.current_slider_links[i])[0]
+            slider_value = self.sliders.itemAt(i).widget().value()
+            change_vector -= values * slider_value
+            sub_vectors = np.vstack((sub_vectors, values))
+        # run
+        index, setValue = Adviser.run(change_vector,sub_vectors[1:])
+        if setValue == None:
+            return None
+        if len(self.current_slider_links) == 1:
+            index = self.slider_links.index(self.current_slider_links[0])
+        # update 
+        self.sliders.itemAt(index).widget().setValue(setValue)
+
+    def auto_max(self):
+        setValueMax = 0
+        indexMax = -1
+        for index in range(len(self.current_slider_links)):
+            change_vector = self.IdealValues*4-self.soilValues
+            values = Files.run(self.current_slider_links[index])[0]
+            setValue = np.amin(change_vector/values)
+            if setValueMax<setValue:
+                setValueMax = setValue  
+                indexMax = index
+        if len(self.current_slider_links) == 1:
+            indexMax = self.slider_links.index(self.current_slider_links[0])
+        print(setValueMax)
+        self.sliders.itemAt(indexMax).widget().setValue(setValueMax)
+                
 class DragDrop(QLineEdit):
     def __init__(self):
-        # inherit
         super(DragDrop, self).__init__()
         self.setGeometry(200,200,200,200)
         self.setStyleSheet("QWidget { background-color: "+MainWindow.color+" }")
@@ -384,17 +394,14 @@ class DragDrop(QLineEdit):
     def dropEvent(self, event):
         data = event.mimeData()
         urls = data.urls()
-        if urls and urls[0].scheme() == 'file':
-            full_paths = []
-            for x in range(len(urls)):
-                url = str(urls[x].path())[1:]
-                if url[-5:].upper() == ".XLSX":
-                    full_paths.append(url)
-            MainWindow.Open(full_paths)
-            self.close()
+        for x in range(len(urls)):
+            url = str(urls[x].path())[1:]
+            if url[-5:].upper() == ".XLSX":
+                full_paths.append(url)
+        MainWindow.Open(full_paths)
+        self.close()
 
 if __name__=='__main__':
-    # close all windows after main
     app = QApplication(sys.argv)
     MainWindow = MainWindow()
     MainWindow.show()
