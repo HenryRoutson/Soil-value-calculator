77884bdc82a02efc0a98d0cc10456f1f5e64ffa6
routson01 <HenryRo@bgs.vic.edu.au>
2019-08-10 21:41:10 +1000

    Comments and bugs found

--- PythonFiles/UserInterface.py
+++ PythonFiles/UserInterface.py
@@ -1,10 +1,11 @@
 """
 to do :
+fix matplotlib update
+test text try
 fix max and ideal
-closeall after main
 fix blinking
+closeall after main
 pep8 http://pep8online.com/checkresult
-comments
 """
 
 import os
@@ -33,13 +34,12 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
 
-        "preferences"
+        "MainWindow preferences"
 
         # look
-        self.text_size_value = 9
-        self.main_color = 0 # += 1 for default
+        self.text_size = 9
+        self.main_color = 0 # the next index color is set
         self.main_colors = ["#9c9c9c", "#EBEBEB"]
-
         self.setWindowTitle('GUI')
         self.setWindowIcon(QIcon('pythonlogo.png'))
         # size
@@ -73,12 +73,12 @@ class MainWindow(QMainWindow):
         SettingsMenu = MainMenu.addMenu('Settings')
 
         text_size_Up = QAction("Text Size Up",self)
-        text_size_Up.triggered.connect(self.text_size)
+        text_size_Up.triggered.connect(self.change_text_size)
         SettingsMenu.addAction(text_size_Up)
 
         self.font = QFont()
         text_size_Down = QAction("Text Size Down",self)
-        text_size_Down.triggered.connect(self.text_size)
+        text_size_Down.triggered.connect(self.change_text_size)
         SettingsMenu.addAction(text_size_Down)
 
         self.light_dark()
@@ -98,38 +98,47 @@ class MainWindow(QMainWindow):
         # self.Open(['C:/Users/henryro/OneDrive - Ballarat Grammar School/2019 Software/Sat/ExcelFilesTesting/Compost.xlsx', 'C:/Users/henryro/OneDrive - Ballarat Grammar School/2019 Software/Sat/ExcelFilesTesting/Ideal.xlsx', 'C:/Users/henryro/OneDrive - Ballarat Grammar School/2019 Software/Sat/ExcelFilesTesting/Soil.xlsx'])
 
     def Open(self, full_paths):
+
+        # if there are no paths from drag drop
         if full_paths == False:
+            # open the file menu
             full_paths = QFileDialog.getOpenFileNames(self,'Open File')[0]
+
+        # delete non xlsx (excel) files
         for i, url in enumerate(full_paths):
             if url[-5:].upper() != ".XLSX":
                 del full_paths[i]
+
+        # copy paths to AllExcelFilesBackup
+        # update paths for program
         for full_path in full_paths:
             shutil.copy(full_path, "AllExcelFilesBackup")
             name =  os.path.basename(full_path)
             if re.search("IDEAL", name.upper()):
-                self.Widgets.ideal_link = full_path
+                self.Widgets.ideal_path = full_path
             elif re.search("SOIL", name.upper()):
-                self.Widgets.soil_link = full_path
+                self.Widgets.soil_path = full_path
             else:
                 self.Widgets.create_slider(full_path)
-        if self.Widgets.soil_link != r"" and self.Widgets.ideal_link != r"":
+
+        # initialise the graph
+        if self.Widgets.soil_path != r"" and self.Widgets.ideal_path != r"":
             self.Widgets.init_graph()
 
     def DragAndDrop(self):
         self.DragDrop = DragDrop()
 
-    def text_size(self):
+    def change_text_size(self):
+
         change = 1
-        try:
-            if self.sender().text() == "Text Size Up":
-                self.text_size_value += change
-            elif self.sender().text() == "Text Size Down":
-                if self.text_size>change:
-                    self.text_size_value += -change
-        except:
-            pass
-        self.Widgets.setStyleSheet("font: "+str(self.text_size_value)+"pt")
-        rcParams.update({'font.size': self.text_size_value + 3})
+        if self.sender().text() == "Text Size Up":
+            self.text_size += change
+        elif self.sender().text() == "Text Size Down":
+            if self.text_size>change:
+                self.text_size += -change
+
+        self.Widgets.setStyleSheet("font: "+str(self.text_size)+"pt")
+        rcParams.update({'font.size': self.text_size + 3})
 
     def light_dark(self):
         if self.main_color >= len(self.main_colors)-1:
@@ -142,23 +151,26 @@ class MainWindow(QMainWindow):
         self.setStyleSheet("QWidget { background-color: "+self.main_colors[self.main_color]+" }")
         self.Widgets.ax.set_facecolor(self.main_colors[self.main_color])
         self.Widgets.fig.set_facecolor(self.main_colors[self.main_color])
-        self.Widgets.init_graph()
 
 class QCustomSlider(QSlider):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        # super is used to refer to the QSlider parent
         self.max_float = 0.05
         super().setMinimum(0)
         super().setMaximum(500)
         
+    # used for directly setting float values
     def value(self):
         return super().value() / super().maximum() * self.max_float
 
     def setValue(self, value):
         super().setValue( int( value / self.max_float * super().maximum() ) )
 
+    # allows click to position 
+    # mousePressEvent code is from:
     # https://github.com/PyQt5/PyQt/blob/master/QSlider/ClickJumpSlider.py
     def mousePressEvent(self, event):
         if event.button() == Qt.LeftButton:
@@ -179,7 +191,7 @@ class Widgets(QWidget):
     def __init__(self, parent=MainWindow):
         super().__init__(parent)
 
-        "preferences"
+        "Widgets preferences"
 
         # functional
         self.max_div_ideal = 4
@@ -205,8 +217,8 @@ class Widgets(QWidget):
         self.slider_layout.addLayout(self.buttons)
         self.slider_layout.addLayout(self.labels)
 
-        self.all_slider_links = []
-        self.slider_links = []
+        self.all_slider_paths = []
+        self.slider_paths = []
         self.all_slider_colors = []
         self.slider_colors = []
         self.slider_color_pos = 0
@@ -219,17 +231,23 @@ class Widgets(QWidget):
 
     def create_slider(self,path):
 
-        for x in self.all_slider_links:
+        # avoid duplicates
+        for x in self.all_slider_paths:
             if path == x:
                 return None
             
-        self.all_slider_links.append(path)
-        self.slider_links.append(path)
+        self.all_slider_paths.append(path)
+        # slider_paths is subset of all_slider_paths
+        # and is used to get ideal values for example, on a single slider
+        self.slider_paths.append(path)
+
         slider = QSlider(Qt.Vertical)
         custom_slider = QCustomSlider(slider)
 
+        # go to start if out of colors
         if self.slider_color_pos == len(self.all_slider_colors)-1:
             self.slider_color_pos = 0
+
         hex_color = self.all_slider_colors[self.slider_color_pos]
         self.slider_color_pos += 1
         self.slider_colors.append(hex_color)
@@ -251,9 +269,9 @@ class Widgets(QWidget):
         self.buttons.itemAt(index).widget().setParent(None)
         self.sliders.itemAt(index).widget().setParent(None)
         self.labels.itemAt(index).widget().setParent(None)
-        self.bars[index].remove()
-        del self.bars[index]
-        del self.all_slider_links[index]
+        self.bars[index].remove() # on graph
+        del self.bars[index] # in memory
+        del self.all_slider_paths[index]
         del self.slider_colors[index]
 
     def graph_init(self):
@@ -267,49 +285,53 @@ class Widgets(QWidget):
 
         toolbar = NavigationToolbar2QT(canvas, self)
         self.graph.addWidget(toolbar)
-
-        self.soil_link = r""
-        self.ideal_link = r""
-
-    def init_graph(self):  
         self.ax = canvas.figure.subplots()
-        self.bars = []
-        xs = np.arange(len(Files.values(self.soil_link)[0]))
-        ys = np.zeros(len(Files.values(self.soil_link)[0]))
+        self.soil_path = r""
+        self.ideal_path = r""
 
-        for i, slider_link in enumerate(self.all_slider_links):
+    def init_graph(self): 
+        # np.arrays help performance 
+        self.bars = []
+        length = len(Files.values(self.soil_path)[0])
+        xs = np.arange(length) # 1,2,3
+        ys = np.zeros(length) # 0,0,0
+
+        for i, slider_path in enumerate(self.all_slider_paths):
             self.bars.append(self.ax.bar(xs, ys, color=self.slider_colors[i], edgecolor='black'))
             
         # using real values sets a propper frame
-        self.bars.append(self.ax.bar(xs, Files.values(self.soil_link)[0], color = "grey"))
-        self.bars.append(self.ax.bar(xs, Files.values(self.ideal_link)[0], facecolor="None", edgecolor='green'))
-        self.bars.append(self.ax.bar(xs, Files.values(self.ideal_link)[0]*self.max_div_ideal, facecolor="None", edgecolor='red'))
+        self.bars.append(self.ax.bar(xs, Files.values(self.soil_path)[0], color = "grey"))
+        self.bars.append(self.ax.bar(xs, Files.values(self.ideal_path)[0], facecolor="None", edgecolor='green'))
+        self.bars.append(self.ax.bar(xs, Files.values(self.ideal_path)[0]*self.max_div_ideal, facecolor="None", edgecolor='red'))
         self.ax.set_xticks(xs)
-        self.ax.set_xticklabels(Files.values(self.soil_link)[1])
+        self.ax.set_xticklabels(Files.values(self.soil_path)[1])
 
+        # blit avoids redrawing and helps performance
         self.FuncAnimation = animation.FuncAnimation(self.fig,self.update_graph,interval=0,blit=True)
 
     def update_values(self, bars, values, bottom = []):
         for i, bar in enumerate(bars):
             bar.set_height(values[i])
+        # set y raises the bar on top of the last
         if bottom != []:
             for i, bar in enumerate(bars):
                 bar.set_y(bottom[i])
 
     def update_graph(self, frames):
-        bottom = Files.values(self.soil_link)[0]
-        for i, slider_link in enumerate(self.all_slider_links):
+        bottom = Files.values(self.soil_path)[0]
+        for i, slider_path in enumerate(self.all_slider_paths):
             slider_value = self.sliders.itemAt(i).widget().value()
-            ys = Files.values(slider_link)[0] * slider_value
+            ys = Files.values(slider_path)[0] * slider_value
             self.update_values(self.bars[i], ys ,bottom)
             bottom += ys
             setText = str(round(self.label_conversion*slider_value,1)) + self.label_unit
             self.labels.itemAt(i).widget().setText(setText)
 
-        self.update_values(self.bars[-3], Files.values(self.soil_link)[0])
-        self.update_values(self.bars[-2], Files.values(self.ideal_link)[0])
-        self.update_values(self.bars[-1], Files.values(self.ideal_link)[0]*self.max_div_ideal)
+        self.update_values(self.bars[-3], Files.values(self.soil_path)[0])
+        self.update_values(self.bars[-2], Files.values(self.ideal_path)[0])
+        self.update_values(self.bars[-1], Files.values(self.ideal_path)[0]*self.max_div_ideal)
 
+        # returns single array of all subarrays to FuncAnimation
         return np.array(self.bars).ravel()
         
     def context_menu_init(self):
@@ -328,25 +350,31 @@ class Widgets(QWidget):
         self.context_menu.addAction(max_values)
 
     def contextMenuEvent(self, event):
-        self.slider_links = self.all_slider_links
+        """
+        if contextMenuEvent is above a slider button
+        set slider_paths to that slider_path
+        so ideal_values for example, will only run over that path
+        without needing a separate single path function
+        """
+        self.slider_paths = self.all_slider_paths
         for index in range(self.buttons.count()):
             widget = self.buttons.itemAt(index).widget().geometry()
             if event.globalX() < widget.x() + widget.width():
-                if index < len(self.all_slider_links):
-                    self.slider_links = [self.all_slider_links[index]]
+                if index < len(self.all_slider_paths):
+                    self.slider_paths = [self.all_slider_paths[index]]
                 break
         self.context_menu.exec_(self.mapToGlobal(event.pos()))
 
     def reset_values(self):
-        for i, slider_link in enumerate(self.slider_links):
-            index = self.all_slider_links.index(slider_link)
+        for i, slider_path in enumerate(self.slider_paths):
+            index = self.all_slider_paths.index(slider_path)
             self.sliders.itemAt(index).widget().setValue(0)
 
     def get_vectors(self, scalar):
-        change_vector = Files.values(self.ideal_link)[0] * scalar - Files.values(self.soil_link)[0]
+        change_vector = Files.values(self.ideal_path)[0] * scalar - Files.values(self.soil_path)[0]
         sub_vectors = np.zeros(len(change_vector)) 
-        for i, slider_link in enumerate(self.slider_links):
-            values = Files.values(slider_link)[0]
+        for i, slider_path in enumerate(self.slider_paths):
+            values = Files.values(slider_path)[0]
             slider_value = self.sliders.itemAt(i).widget().value()
             change_vector -= values * slider_value
             sub_vectors = np.vstack((sub_vectors, values))
@@ -354,31 +382,44 @@ class Widgets(QWidget):
 
     def ideal_values(self):
         change_vector, sub_vectors = self.get_vectors(1)
+        
         index, setValue = Adviser.run(change_vector,sub_vectors)
+        # if the advisor doesn't have a change to improve
         if setValue == None:
+            # exit
             return None
-        if len(self.slider_links) == 1:
-            index = self.all_slider_links.index(self.slider_links[0])
+
+        # if this is a contextMenuEvent path
+        # eg [[path]]
+        if len(self.slider_paths) == 1:
+            # make sure its index isn't always 0
+            index = self.all_slider_paths.index(self.slider_paths[0])
+
         self.sliders.itemAt(index).widget().setValue(setValue)
 
     def max_values(self):
         change_vector, sub_vectors = self.get_vectors(self.max_div_ideal)
         setValue, index = 0, 0 
+
+        # find the largest distance one of the sliders can go before going over the limits
         for i, sub_vector in enumerate(sub_vectors):
             temp = np.amin(change_vector/sub_vector)
             if setValue < temp:
                 setValue = temp  
                 index = i
-        if len(self.slider_links) == 1:
-            index = self.all_slider_links.index(self.slider_links[0])
+
+        if len(self.slider_paths) == 1:
+            index = self.all_slider_paths.index(self.slider_paths[0])
+
+        # add the value the slider is already on and set
         setValue += self.sliders.itemAt(index).widget().value()
         self.sliders.itemAt(index).widget().setValue(setValue)
    
 class DragDrop(QLineEdit):
     def __init__(self):
         super(DragDrop, self).__init__()
+        # dark mode not included for simplicity
         self.setGeometry(200,200,200,200)
-        self.setStyleSheet("QWidget { background-color: "+MainWindow.color+" }")
         self.setText("Drag and drop here")
         self.setDragEnabled(True)
         self.show()
@@ -392,6 +433,7 @@ class DragDrop(QLineEdit):
     def dropEvent(self, event):
         data = event.mimeData()
         urls = data.urls()
+        full_paths = []
         for x, url in enumerate(urls):
             full_paths.append(str(url.path())[1:])
         MainWindow.Open(full_paths)
